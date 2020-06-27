6aa6541810397cf18dc828c06d4e42a4d095aba1
Eman Sayma <eman.sayma@photobookworldwide.com>
2020-06-27 22:16:08 +0800

    update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 exports.printMe =  (color) => {
-    if (!color) color = '#ffeeee';
-    console.log('%c Oh my heavens! ', ' color: [color]');
+    console.log(`Hi, I'm ${colorTextLog('cyan color', color)}!`);
 }
+
+ colorTextLog = (text, color) => { return `\x1b[${color}m${text}\x1b[0m`; }
